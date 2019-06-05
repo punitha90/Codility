232b8c2eead7e93051b928316c5f3b142704da27
punitha90 <punithamantra77@gmail.com>
2019-06-05 17:00:57 +0100

    MissingInteger Commit

--- src/MissingInteger.java
+++ src/MissingInteger.java
@@ -1,40 +1,20 @@
-/*Task description
-An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+/*Write a function:
 
-The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+class Solution { public int solution(int[] A); }
 
-Write a function:
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
 
-class Solution { public int[] solution(int[] A, int K); }
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
 
-that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+Given A = [1, 2, 3], the function should return 4.
 
-For example, given
+Given A = [−1, −3], the function should return 1.
 
-    A = [3, 8, 9, 7, 6]
-    K = 3
-the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+Write an efficient algorithm for the following assumptions:
 
-    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
-    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
-    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
-For another example, given
-
-    A = [0, 0, 0]
-    K = 1
-the function should return [0, 0, 0]
-
-Given
-
-    A = [1, 2, 3, 4]
-    K = 4
-the function should return [1, 2, 3, 4]
-
-Assume that:
-
-N and K are integers within the range [0..100];
-each element of array A is an integer within the range [−1,000..1,000].
-In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.*/
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+*/
 
 
 import java.util.HashSet;
