f605078b7092cd565eb6ba3b07167f62ddca115c
punitha90 <punithamantra77@gmail.com>
2019-06-05 16:59:16 +0100

    Initial commit

---
+++ src/TapeEquilbrium.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+
+Your goal is to find that missing element.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A, returns the value of the missing element.
+
+For example, given array A such that:
+
+  A[0] = 2
+  A[1] = 3
+  A[2] = 1
+  A[3] = 5
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].*/
+public class TapeEquilbrium {
+    public static int Solutions(int A[]) {
+
+        int sumMin = A[0];
+        int sumMax = 0;
+
+        for (int i = 1; i < A.length; i++) {
+            sumMax += A[i];
+        }
+
+        int minDif = Math.abs(sumMin - sumMax);
+        for (int i = 1; i < A.length - 1; i++) {
+            sumMin += A[i];
+            sumMax -= A[i];
+            minDif = Math.min(minDif, Math.abs(sumMin - sumMax));
+        }
+
+        return minDif;
+
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        int[] A = { 3, 1, 2, 4, 3 };
+        System.out.println(Solutions(A));
+    }
+
+}
